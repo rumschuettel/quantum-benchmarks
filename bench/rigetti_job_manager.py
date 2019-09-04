276a2fe84c10846eb1378aceccd2155cb5cd8978
Johannes Bausch <johannes.bausch@gmail.com>
2019-09-04 08:40:13 +0100

    started on rigetti setup

---
+++ bench/rigetti_job_manager.py
@@ -0,0 +1,18 @@
+import pyquil as pq
+
+class JobManager:
+
+
+    @staticmethod
+    def list_devices():
+        print(pq.list_quantum_computers())
+
+    @staticmethod
+    def test():
+        from pyquil.gates import CNOT, Z
+        # to run on real device, change as_qvm=False
+        qvm = pq.get_qc('9q-square', as_qvm=True)
+        prog = pq.Program(Z(0), CNOT(0, 1))
+
+        results = qvm.run_and_measure(prog, trials=10)
+        print(results)
