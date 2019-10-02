3e3fb5560fc06feddaf739e3bd71ed014ae843bc
arjan <arjan_rekenmachine@hotmail.com>
2019-10-02 14:54:22 +0200

    Added update bash script.

---
+++ try_update.sh
@@ -0,0 +1,12 @@
+for f in ./runs/*
+do
+  id=$(basename $f)
+  printf "Checking folder $id..."
+  if [ -f "./runs/$id/collated.pickle" ]
+  then
+    printf " done.\n"
+  else
+    printf " invoking update\n"
+    python3 runner.py resume $id
+  fi
+done
