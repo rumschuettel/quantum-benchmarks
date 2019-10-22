705c0e016d7a34108854c6df20dbb06a20aeb9e0
Johannes Bausch <johannes.bausch@gmail.com>
2019-10-22 18:28:27 +0100

    fixed thin promise bug

--- libbench/link.py
+++ libbench/link.py
@@ -14,6 +14,7 @@ class ThinPromise(ABC):
         try:
             self._result = result_callback(*args, **kwargs)
         except:
+            self._result = None
             pass
 
     def job_id(self):
