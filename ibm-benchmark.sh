b020bb9d76e868520a8992376d4f6a6595ac1388
Johannes Bausch <johannes.bausch@gmail.com>
2020-09-07 14:59:07 +0100

    updated ibm benchmark list

--- ibm-benchmark.sh
+++ ibm-benchmark.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-DEVICE="$1"
+MODE="$1"
+DEVICE="$2"
 
+echo $MODE
 echo $DEVICE
 
 # run
-#./runner.py benchmark ibm cloud "$DEVICE" Schroedinger-Microscope -ps 1 -p 32 -s 4096
-./runner.py benchmark ibm cloud "$DEVICE" Schroedinger-Microscope -ps 2 -p 32 -s 4096
-#./runner.py benchmark ibm cloud "$DEVICE" Mandelbrot -ps 1 -p 32 -s 4096
-./runner.py benchmark ibm cloud "$DEVICE" Mandelbrot -ps 2 -p 32 -s 4096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Schroedinger-Microscope -ps 1 -p 32 -s 4096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Schroedinger-Microscope -ps 2 -p 32 -s 4096
+
+./runner.py benchmark ibm "$MODE" "$DEVICE" Mandelbrot -ps 1 -p 32 -s 4096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Mandelbrot -ps 2 -p 32 -s 4096
+
+./runner.py benchmark ibm "$MODE" "$DEVICE" Platonic-Fractals -t 1 -m 32 -s 8096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Platonic-Fractals -t 2 -m 32 -s 8096
+
+./runner.py benchmark ibm "$MODE" "$DEVICE" Line-Drawing -n 2 -r 25 -s 8096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Line-Drawing -n 4 -r 25 -s 8096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Line-Drawing -n 8 -r 25 -s 8096
+./runner.py benchmark ibm "$MODE" "$DEVICE" Line-Drawing -n 16 -r 25 -s 8096
+
+./runner.py benchmark ibm "$MODE" "$DEVICE" Bell-Test -s 8096
