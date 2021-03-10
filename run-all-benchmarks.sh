c13c324cf3ff1b6ce28d81ac903f87e95d775451
Andris <gilyenandras@gmail.com>
2021-03-09 16:32:10 -0800

    HHL benchmarks added to run-all script

--- run-all-benchmarks.sh
+++ run-all-benchmarks.sh
@@ -9,11 +9,11 @@ echo "$VENDOR, $MODE, $DEVICE"
 # run
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Bell-Test -s 8192
 
-#./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Schroedinger-Microscope -ps 1 -p 32 -s 4096
-#./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Schroedinger-Microscope -ps 2 -p 32 -s 4096
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Schroedinger-Microscope -ps 1 -p 32 -s 4096
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Schroedinger-Microscope -ps 2 -p 32 -s 4096
 
-#./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Mandelbrot -ps 1 -p 32 -s 4096
-#./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Mandelbrot -ps 2 -p 32 -s 4096
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Mandelbrot -ps 1 -p 32 -s 4096
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Mandelbrot -ps 2 -p 32 -s 4096
 
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Platonic-Fractals -t 1 -m 32 -s 8192
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Platonic-Fractals -t 2 -m 32 -s 8192
@@ -22,4 +22,11 @@ echo "$VENDOR, $MODE, $DEVICE"
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Line-Drawing -n 4 -r 25 -s 8192
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Line-Drawing -n 8 -r 25 -s 8192
 ./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" Line-Drawing -n 16 -r 25 -s 8192
+
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 2qubit-1ancilla-CZ -s 8192
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 3qubit-1ancilla-CZ -s 8192
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 4qubit-1ancilla-CZ -s 8192
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 5qubit-1ancilla-CZ -s 8192
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 6qubit-1ancilla-CX -s 1024
+./runner.py benchmark "$VENDOR" "$MODE" "$DEVICE" HHL -c 7qubit-1ancilla-CX -s 1024
 
